b30f7a300b14650bc91bb6f15eb13fc2ff2d2e27
dwalkz <dwalkergormley@gmail.com>
2016-08-04 12:42:07 -0400

    Refactored code into a portable library object callable on any HTML image element. Implemented "Quick" pixelate algorithm

--- README.md
+++ README.md
@@ -1,10 +1,62 @@
-# Mozaiker
-An educational exercise in javascript to test capabilities of image data manipulation
+# Modiphy
+What began as an educational exercise in manipulating pixel data in the HTML5 canvas, I'm now turning into a full fledged Image Manipulation Library.
 
-I simply attempted this because I had the idea for an algorithm to copy the  "mosaic tile" filter found in popular image editing programs like Photoshop and Gimp. The idea is that these filters break an image into discrete tiles and use a dominant color in that tile. In this way, an image with less tiles will be less detailed and the more tiles you have, the more detailed you get. 
+Current functionality is limited only to pixelating an image into square pixels, but I'm planning on releasing updates to use differently shaped pixels (square, diamond, circle, triangle, hexagonal), create different filters (blur, color shifts, inverse, swirl, "glitch", and more)
 
-The algorithm takes any image input by a user and breaks it into even tiles based on a slider input. It will then find an average RGB value for the pixels in that block and then set the block to that average value and redraw it on an HTML5 canvas on the fly. 
+My hope is to turn this into a relatively light-weight image manipulation library that can be used to provide on-the-fly pure javascript based image filters and show off some of the theoretical capabilities of the Canvas API for editing pixel data.
 
-As of first versioning, tiling a large image (1600x1200 72dpi) into 200 even tiles takes about 1.5 seconds on average, but several improvements to this are in the works. At some point, I plan on abstracting these features into an image manipulation library that can be used to duplicate various other features, perhaps like blurring, cartoonizing, edge detection, etc. 
+## Minimum Usage
+``` js
+var img = document.getElementById("imgElem");
+var mod = new Modiphy(img);
+```
+Simple as that!
+ 
+## Example Code
+This script works on any _same domain image_. This restriction is due to HTML5 Canvas restrictions on cross-domain content and using getImageData. You can read the spec [here](http://dev.w3.org/html5/spec/the-canvas-element.html#security-with-canvas-elements).
 
-You can test this script and see how it's being used by simply downloading the three files and opening the index.html file in your preferred browser (tested in Chrome). 
+Use the library by grabbing your image element and creating a new Modiphy object with it
+``` html
+<img id='image1' src='/images/mountains.png' />
+```
+``` js
+//Ensure the image has loaded using addEventListener or some other method before attempting to Modiphy it
+var img = document.getElementById("image1");
+var filtered = new Modiphy(img, {
+    filter: "pixelate",
+    method: "quick",
+    divisor: 50
+});
+```
+This code will replace the image element with a canvas (keeping the same id/class attributes in place) and will pixelate the image using the Quick method and a divisor of 50. 
+
+The above line can also be rewritten as ` filtered = new Modiphy(img) ` since the options used are already default. 
+
+After a Modiphy object has been created, you can change any options and recreate it.
+``` js
+filtered.render({
+   method: "average",
+   divisor: 100
+});
+```
+
+## Options
+The options that can be passed to Modiphy are as follows:
+``` 
+filter: Currently "pixelate" is the only option but we're looking into adding 
+        blurs, color shifts, glitches, and many other styles.
+        
+method: Combined with pixelate, there's two methods available:
+        "quick"   - (default) Uses a fast, cheap algorithm to pixelate without looking 
+                    at all pixels in a new block.
+        "average" - Averages all RGB values in a block to get mathematically accurate
+                    pixelation but takes more time to complete
+                    
+divisor: (int) Defaults to 50. For the pixelate filter, the width and height gets
+               divided by this number to determine the block sizes. E.x. a square
+               image of 500px by 500px and a divisor of 50 means each new pixel block
+               will be 10px by 10px. Higher divisor == more detailed pixels
+            
+debug: (boolean) Defaults to false. Specifies whether verbose output should be output
+                 to the console
+```
---
+++ modiphy.js
@@ -0,0 +1,204 @@
+/*!
+ Library: Modiphy
+ Author: David Gormley
+ Date: 2016-08-06T12:22AM
+ Version: 0.5.1
+
+ Modiphy is an image manipulation library that cna be used on the fly
+ to filter images with common Photoshop like filters by using the
+ HTML5 Canvas API to get and modify pixel data in different ways.
+
+ This library is primarily an educational exercise but is released
+ under the MIT License (Copyright 2016 David Gormley) and provided
+ as is for use. Suggestions welcome at my github:
+ https://github.com/dwalkz/Modiphy
+ */
+(function(window){
+    'use strict';
+
+    var version = "0.5.1";
+    var console = window.console;
+
+    //Check for canvas support
+    var canvas = document.createElement("canvas");
+    var canvasSupport = canvas.getContext && canvas.getContext("2d");
+    //If we can't use the canvas, Modiphy won't be able to do anything
+    if(!canvasSupport) {
+        return;
+    }
+
+    function Modiphy(img, options) {
+        this.img = img;
+        this.options = {            //Set the default options for a Modiphy object
+            filter: "pixelate",     //The filter we want to use on the image
+            method: "quick",        //The algorithm used to pixelate
+            divisor: 50             //Determines the block size. Divide the image into 'n' blocks
+        };
+
+        var canvas = this.canvas = document.createElement("canvas");
+        this.ctx = canvas.getContext("2d");
+        canvas.className = img.className;
+        canvas.id = img.id;
+
+        this.render( options );     //Render (or re-render) the image with a number of options
+
+        //Replace the image with a canvas
+        img.parentNode.replaceChild(canvas, img);
+    }
+
+    Modiphy.prototype.about = function(){
+        alert("Modiphy is currently in development version " + version);
+    };
+
+    /*
+     Merge options passed into this.render with our default options
+     */
+    Modiphy.prototype.updateOptions = function(options) {
+        this.options.filter = options.filter || this.options.filter;
+        this.options.method = options.method || this.options.method;
+        this.options.divisor = options.divisor || this.options.divisor;
+    };
+
+    Modiphy.prototype.render = function( options ) {
+        if(options)
+            this.updateOptions(options);
+        else
+            this.updateOptions({});
+
+        //Draw the image on our canvas
+        this.ctx.drawImage(this.img, 0, 0);
+
+        //Let's determine how we need to filter the image
+        switch(this.options.filter) {
+            case "pixelate":
+                pixelate.call(this);
+                break;
+            default:
+                console.log("No filter or invalid filter given. Pixelating image by default");
+                pixelate.call(this);
+                break;
+        }
+    };
+
+    /*
+     Function called by render when filter == "pixelate" to handle
+     checking which method we want to use to pixelate
+     */
+    function pixelate ( ) {
+        //Determine how we need to pixelate the image whether by using
+        //An average or using the quick pixelate cheating method
+        if(this.options.method == "quick") {
+            pixelateQuick.call(this);
+        } else {
+            pixelateAverage.call(this);
+        }
+    }
+
+    //TODO: Implement different shapes for the output pixels
+    /*!
+     A thorough pixelation method which uses a real average RGB value
+     of all the pixels in each "pixel block" as determined by the divisor.
+
+     This function will loop through each pixel in the block and calculate
+     an average. This process can be time consuming, but is more mathematically
+     accurate because of it.
+     */
+    function pixelateAverage () {
+        if(this.drawing === true) {
+            //If we're already drawing a new image, don't do anything. This prevents resource hogging
+            //Since the "average" method can take quite a long time
+            return;
+        }
+
+        this.drawing = true;
+
+        //Clear the canvas and redraw the original image on top to clear out old data
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.ctx.drawImage(this.img, 0, 0);
+
+        var divisor = this.options.divisor;
+        var pixelWidth = Math.ceil(this.img.width / divisor);
+        var pixelHeight = Math.ceil(this.img.height / divisor);
+
+        var currX = 0, currY = 0;
+
+        //Loop through the image and break it down into pixel blocks of a width and height
+        for(var i = 0; i < Math.pow(divisor, 2); i++) {
+            var currentPixelBlock = this.ctx.getImageData(currX, currY, pixelWidth, pixelHeight).data;
+            //Calculate the average values for this block
+            var rSum = 0, gSum = 0, bSum = 0, numPixels = 0;
+            for(var n = 0; n < currentPixelBlock.length; n+=4) {
+                rSum += currentPixelBlock[ n   ];
+                gSum += currentPixelBlock[ n+1 ];
+                bSum += currentPixelBlock[ n+2 ];
+                //Alpha would be here, but it's not supported now because it's unnecessary
+                numPixels++;
+            }
+
+            var rVal = (rSum / numPixels).toFixed();
+            var gVal = (gSum / numPixels).toFixed();
+            var bVal = (bSum / numPixels).toFixed();
+
+            //Draw a new "pixel" in place with our average value
+            this.ctx.fillStyle = "rgb("+rVal+", "+gVal+", "+bVal+")";
+            this.ctx.fillRect(currX, currY, pixelWidth, pixelHeight);
+            currX += pixelWidth;
+            if(currX >= this.img.width) {
+                currX = 0;
+                currY += pixelHeight
+            }
+        }
+
+        this.drawing = false;
+    }
+
+    //TODO: Implement different shapes for the output pixels
+    /*!
+     A "cheat" way to emulate pixelation by assuming the important color in
+     a given block is always the center-most pixel. Breaks the image down
+     into pixel blocks determined by the divisor and then finds the pixel
+     in the very center of the block.
+
+     Even though this isn't a true average, as the divisor increases, the
+     assumption that the center pixel is the average becomes more true so
+     it's computationally faster and looks close enough.
+
+     This is the default pixelation method for speed
+     */
+    function pixelateQuick (){
+        if(this.drawing === true) {
+            return;
+        }
+
+        this.drawing = true;
+
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.ctx.drawImage(this.img, 0, 0);
+
+        var divisor = this.options.divisor;
+        var pixelWidth = Math.ceil(this.img.width / divisor);
+        var pixelHeight = Math.ceil(this.img.height / divisor);
+
+        var currX = 0, currY = 0;
+
+        for(var i = 0; i < Math.pow(divisor, 2); i++) {
+            var currentPixelBlock = this.ctx.getImageData(currX, currY, pixelWidth, pixelHeight).data;
+            var cpi = ((currentPixelBlock.length / 4 / 2) - 1) * 4;
+            var rVal = currentPixelBlock[ cpi   ];
+            var gVal = currentPixelBlock[ cpi+1 ];
+            var bVal = currentPixelBlock[ cpi+2 ];
+
+            this.ctx.fillStyle = "rgb("+rVal+", "+gVal+", "+bVal+")";
+            this.ctx.fillRect(currX, currY, pixelWidth, pixelHeight);
+            currX += pixelWidth;
+            if(currX >= this.img.width) {
+                currX = 0;
+                currY += pixelHeight
+            }
+        }
+
+        this.drawing = false;
+    }
+
+    window.Modiphy = Modiphy;
+})(window);
